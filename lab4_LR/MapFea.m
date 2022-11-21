2dc956a9622fa3ddbb23e4747d2bfb6d2fcfb30a
KrzysiekJa <kjmj77@gmail.com>
2022-11-21 10:32:48 +0100

    Bug fixed (e.g. SplitData.m). Added basic plotting.

--- lab4_LR/MapFea.m
+++
@@ -1,8 +0,0 @@
-function X = MapFea( X )
-
-% getting 2 powers of variables
-extraRowsUp = ones(1, size(X,2));
-extraRowsDown = X .^ 2;
-X = [extraRowsUp; X; extraRowsDown];
-
-end
--- lab4_LR/ReadData.m
+++
@@ -1,7 +0,0 @@
-function [X, Y] = ReadData
-
-load LC_data.mat X Fm;
-
-Y = Fm; % martensit
-
-end
