17ffac6827656ad03b8e03a8a007179b10c868d1
KrzysiekJa <kjmj77@gmail.com>
2022-12-14 20:33:30 +0100

    Added 4 steps for PCA in RunML5.m

--- lab_8_PCA/RunML5.m
+++ lab_8_PCA/RunML5.m
@@ -6,8 +6,30 @@ disp(' >> Krzysztof Jarek << ');
 
 [X, Y] = ReadData(1);
 
+% >> Step 1
+X_nex = ( X - mean(X, 2) ); % ./ std(X, 0, 2)
 
-%[V,D] = eig(X)
+% >> Step 2
+CovM = cov(X_nex')
+
+% CovM = zeros(size(X_nex,1), size(X_nex,1));
+% for i = 1:size(X_nex,1)
+%     CovM(i,:) = sum( (X_nex(i,:) - mean(X_nex(i,:)) )  * ( X_nex(i,:) - mean(X_nex(i,:)) )' ) / length( (X_nex(i,:) );
+% end
+
+% >> Step 3
+[V, d] = eig(CovM);
+
+% >> Step 4
+[~, ind] = sort(diag(d), 1, "descend");
+d = d(ind, ind)
+V = V(:, ind)
+
+% >> Step 5 -> taking two most variant dims
+X_final = V( :, ind(1:2) )' * X_nex( ind(1:2), : );
+X_final = X_final';
+
+%variance = d / sum(d(:))
 
 
 %     m
