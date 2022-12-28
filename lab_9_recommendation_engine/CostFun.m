4131948ef0d4d618fef1842255f9820976c13eb7
KrzysiekJa <kjmj77@gmail.com>
2022-12-28 02:31:24 +0100

    Complited CostFun.m (not sure about the effects)

--- lab_9_recommendation_engine/CostFun.m
+++ lab_9_recommendation_engine/CostFun.m
@@ -5,16 +5,13 @@ dJ = zeros( 2*nf+1, 1 );
 
 theta = reshape( Theta_X( 1:((nf+1)*nu) ), nf+1, nu);
 X = [ ones(1, np); reshape( Theta_X( ((nf+1)*nu+1):end ), nf, np) ];
-disp([size(theta) size(X)]);
 
 J = 0.5 * sum( ( (theta' * X).*R - Y.*R) .^2, "all") + (0.5*lambda) * ( sum( theta.^ 2, "all") + sum( X.^ 2, "all") );
 
-disp(J);
-
-dJ(1)        = sum( ( theta(1,:)' * X(1,:) ).*R - Y.*R, "all");
-mutual_part  = sum( ( theta(2:end,:)' * X(2:end,:) ).*R - Y.*R, "all");
-dJ(2:nf+1)   = mutual_part * X(2:end,:)     + lambda * sum( theta(2:end,:), "all");
-dJ(nf+2:end) = mutual_part * theta(2:end,:) + lambda * sum( X(2:end,:), "all");
+dJ(1)        = sum( ( theta(1,:)' * X(1,:) ).*R - Y.*R, "all" );
+mutual_part  = ( theta(2:end,:)' * X(2:end,:) ).*R - Y.*R;
+dJ(2:nf+1)   = sum( mutual_part' * theta(2:end,:)' )' + lambda * sum( theta(2:end,:), 2 );
+dJ(nf+2:end) = sum( mutual_part * X(2:end,:)' )' + lambda * sum( X(2:end,:), 2 );
 
 end
 
